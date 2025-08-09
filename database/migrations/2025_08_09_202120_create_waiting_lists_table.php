695990694d935c55e05b26aa2e4b5a81279ed23b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-08-09 22:19:34 +0100

    Updates

--- database/migrations/2025_08_09_202120_create_waiting_lists_table.php
+++ database/migrations/2025_08_09_202120_create_waiting_lists_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('waiting_lists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('customer_id')->constrained();
+            $table->string('service');
+            $table->enum('status', ['waiting', 'notified'])->default('waiting');
             $table->timestamps();
         });
     }
