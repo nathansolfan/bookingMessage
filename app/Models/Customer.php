dac9b2788dc787917bf1a4d755c6ba75089a7bd2
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-08-09 22:57:12 +0100

    Updates

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    //customer BELONGSTO an User and HASMANY WaitlingLists
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function waitingLists()
+    {
+        $this->hasMany(WaitingList::class);
+    }
+
 }
